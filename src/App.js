e9a84827ad9a25b8993d61a2a1f8df6489885589
Alex Cai <yhcai@ucdavis.edu>
2020-08-14 21:58:01 -0700

    initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+    }
+  }
+
+  
+
+  handleSubmitIncrease = e => {
+    this.setState( {counter: this.state.counter + 1} )
+    e.preventDefault(); // prevent Default HTML action
+    
+  };
+
+  handleSubmitDecrease = e => {
+    this.setState( {counter: this.state.counter - 1} )
+    e.preventDefault(); // prevent Default HTML action
+    
+  };
+
+  render() {
+    const formStyle = {
+      margin: 'auto',
+      padding: '50px',
+    };
+    const rowStyle = {
+      margin: '10px',
+      width: '300px',
+      height: '100px',
+      padding: '10px',
+    };
+    console.log(this.state.counter)
+    const {abc} = this.state;
+    return (
+      <div className ="App">
+        <h1 className ="showcounter">
+          {this.state.counter}
+        </h1>
+        <button onClick = {this.handleSubmitIncrease} className = 'counter'>
+          increase
+          </button>
+        <button onClick = {this.handleSubmitDecrease} className = 'counter'>
+          decrease
+          </button>
+         
+      </div>
+    );
+  }
+}
+//aaaa
 export default App;
